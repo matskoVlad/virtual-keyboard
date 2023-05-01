4c23b52c6c0b443541c2434e5faabc8d4ddb43ed
matskoVlad <thyartismurder121200@gmail.com>
2023-05-01 17:26:45 +0700

    feat: add light for active button

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 //создать массив на английском языке
 const keyboardLayoutEN = [
     'Tab', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=',
-    'CapsLk', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']',
+    'CapsLock', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']',
     'Shift', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '\'', '\\', 
     'Ctrl', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/','Space', 'Backspace', 
+    'Alt'
+  ];
+const keyboardLayoutRU = [
+    'Tab', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=',
+    'CapsLock', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', '[', ']',
+    'Shift', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', ';', '\'', '\\', 
+    'Ctrl', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', ',', '.', '/','Space', 'Backspace', 
     'Alt'
   ];
 
@@ -25,7 +32,6 @@ createKeyboard()
 const inputText = document.querySelector('input')
 let keysArray = document.querySelectorAll('.oneKey')
 keysArray.forEach((el) => {
-    
     el.addEventListener('click', (ev) => {
         console.log(el.id)
         if (ev.target.id.length === 1){
@@ -33,9 +39,10 @@ keysArray.forEach((el) => {
         }
     })
 })
-// реализуем Backspace
+// реализуем 'Backspace'
 const backspace = document.getElementById('Backspace')
-backspace.addEventListener('click', ()=> {
+backspace.addEventListener('click', (event)=> {
+    console.log(event.id)
     let text = inputText.value
     inputText.value = text.slice(0, -1)
 })
@@ -44,4 +51,73 @@ const space = document.getElementById('Space')
 space.addEventListener('click', ()=> {
     let text = inputText.value
     inputText.value = text + ' '
+})
+//добавление функционала "tab"
+const tab = document.getElementById('Tab')
+tab.addEventListener('click', ()=> {
+    let text = inputText.value
+    inputText.value = text + '    '
+})
+
+// Отслеживаем событие нажатия на кнопку
+document.addEventListener('keydown', (event) => {
+    console.log(event.key)
+    if (event.key.length === 1){
+        inputText.value += event.key.toUpperCase()
+        let pressedKey = document.getElementById(event.key.toUpperCase())
+        pressedKey.classList.add('keyPress')
+    } else if (event.key == 'Backspace') {
+        let text = inputText.value
+        inputText.value = text.slice(0, -1)
+        let pressedKey = document.getElementById(event.key)
+        pressedKey.classList.add('keyPress')
+    } else if (event.code == 'Space') {
+        console.log(event.code)
+        let text = inputText.value
+        inputText.value = text + ' '
+        let pressedKey = document.getElementById('Space')
+        pressedKey.classList.add('keyPress')
+    } else if (event.key == 'Tab'){
+        let pressedKey = document.getElementById(event.key)
+        pressedKey.classList.add('keyPress')
+    } else if (event.key == 'Shift'){
+        let pressedKey = document.getElementById(event.key)
+        pressedKey.classList.add('keyPress')
+    } else if (event.key == 'Control'){
+        let pressedKey = document.getElementById('Ctrl')
+        pressedKey.classList.add('keyPress')
+    } else if (event.key == 'Alt'){
+        let pressedKey = document.getElementById(event.key)
+        pressedKey.classList.add('keyPress')
+    } else if (event.key == 'CapsLock'){
+        let pressedKey = document.getElementById(event.key)
+        pressedKey.classList.toggle('keyPress')
+    }
+})
+
+document.addEventListener('keyup', (event) => {
+    if (event.key.length === 1){
+        let pressedKey = document.getElementById(event.key.toUpperCase())
+        pressedKey.classList.remove('keyPress')
+    } else if (event.key == 'Backspace') {
+        let pressedKey = document.getElementById(event.key)
+        pressedKey.classList.remove('keyPress')
+    } else if (event.code == 'Space') {
+        console.log(event.code)
+        let pressedKey = document.getElementById('Space')
+        pressedKey.classList.remove('keyPress')
+    } else if (event.key == 'Tab'){
+        let pressedKey = document.getElementById(event.key)
+        pressedKey.classList.remove('keyPress')
+    } else if (event.key == 'Shift'){
+        let pressedKey = document.getElementById(event.key)
+        pressedKey.classList.remove('keyPress')
+    } else if (event.key == 'Control'){
+        let pressedKey = document.getElementById('Ctrl')
+        pressedKey.classList.remove('keyPress')
+    } else if (event.key == 'Alt'){
+        let pressedKey = document.getElementById(event.key)
+        pressedKey.classList.remove('keyPress')
+    }
+
 })
