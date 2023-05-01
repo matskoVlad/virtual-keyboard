bdcc2d83a6ff80ae993b14bef020fe228882c6d0
matskoVlad <thyartismurder121200@gmail.com>
2023-05-01 19:56:49 +0700

    refactor: delete comments

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//создать массив на английском языке
+
 const keyboardLayoutEN = [
     'Tab', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=',
     'CapsLock', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']',
@@ -19,7 +19,7 @@ document.body.insertAdjacentHTML('beforeend', '<div class="keys"></div>');
 let currentLang = 'en';
 let currentKeyb = keyboardLayoutEN;
 
-// создаем клавиши в html
+
 let createKeyboard = (currentKeyb) => {
     let spaceForKeyboard = document.querySelector('.keys');
     let res = '';
@@ -28,9 +28,9 @@ let createKeyboard = (currentKeyb) => {
         spaceForKeyboard.innerHTML = res;
     })
 }
-// запускам функцию
+
 createKeyboard(currentKeyb);
-// отслеживаем инпут и прописываем функцию передающую при клике по кнопке значение id  в поле ввода текста
+
 
 function functionalButtons(){
     const inputText = document.querySelector('input');
@@ -43,41 +43,41 @@ keysArray.forEach((el) => {
         }
     })
 })
-    // реализуем 'Backspace'
+
 const backspace = document.getElementById('Backspace')
 backspace.addEventListener('click', (event)=> {
     console.log(event.id)
     let text = inputText.value
     inputText.value = text.slice(0, -1)
 })
-//добавление функционала "space"
+
 const space = document.getElementById('Space')
 space.addEventListener('click', ()=> {
     let text = inputText.value
     inputText.value = text + ' '
 })
-//добавление функционала "tab"
+
 const tab = document.getElementById('Tab')
 tab.addEventListener('click', ()=> {
     let text = inputText.value
     inputText.value = text + '    '
 })
-// Отслеживаем событие нажатия на кнопку
+
 document.addEventListener('keydown', (event) => {
     console.log(event.key)
     if (event.key.length === 1){
-        // inputText.value += event.key.toUpperCase()
+ 
         let pressedKey = document.getElementById(event.key.toUpperCase())
         pressedKey.classList.add('keyPress')
     } else if (event.key == 'Backspace') {
         let text = inputText.value
-        // inputText.value = text.slice(0, -1)
+
         let pressedKey = document.getElementById(event.key)
         pressedKey.classList.add('keyPress')
     } else if (event.code == 'Space') {
         console.log(event.code)
         let text = inputText.value
-        // inputText.value = text + ' '
+
         let pressedKey = document.getElementById('Space')
         pressedKey.classList.add('keyPress')
     } else if (event.key == 'Tab'){
@@ -97,7 +97,7 @@ document.addEventListener('keydown', (event) => {
         pressedKey.classList.toggle('keyPress')
     }
 })
-// Отмена нажатия на кнопку
+
 document.addEventListener('keyup', (event) => {
     if (event.key.length === 1){
         let pressedKey = document.getElementById(event.key.toUpperCase())
